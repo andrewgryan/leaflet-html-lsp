062add1728fdbf2e7ebb55738b14c538560deb16
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-06-10 18:12:31 +0100

    regular commit

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,15 @@ interface Message {
 
 interface RequestMessage extends Message {
   id: number;
-  method: string;
+  method: "initialize" | "shutdown" | "exit";
 }
 
+type Notification =
+  | {method: "document/didOpen", params: DidOpenTextDocumentParams}
+  | {method: "document/didChange", params: DidChangeTextDocumentParams}
+
 // Decode message
-export const decode = (content: string): RequestMessage | null => {
+export const decode = (content: string): Notification | RequestMessage | null => {
   const [header, body] = content.split("\r\n\r\n")
   if (header.startsWith("Content-Length")) {
     const length = parseInt(header.split(": ")[1])
@@ -56,15 +60,67 @@ const initializeResult = () => {
 
 const shutdownResult = () => null
 
+type DocumentStore = {
+  [key: string]: string;
+}
+
+const DOCUMENTS: DocumentStore = {}
+
+interface TextDocumentItem {
+  uri: string;
+  languageId: string;
+  version: number;
+  text: string;
+}
+
+interface DidOpenTextDocumentParams {
+  textDocument: TextDocumentItem;
+}
+
+interface DidChangeTextDocumentParams {
+  textDocument: VersionedTextDocumentIdentifier;
+  contentChanges: TextDocumentContentChangeEvent[];
+}
+
+interface VersionedTextDocumentIdentifier extends TextDocumentIdentifier {
+  version: number;
+}
+
+interface TextDocumentIdentifier {
+  uri: string;
+}
+
+type TextDocumentContentChangeEvent = {
+  // TODO: implement other properties
+  text: string;
+}
+
+const documentDidOpen = (params: DidOpenTextDocumentParams) => {
+  DOCUMENTS[params.textDocument.uri] = params.textDocument.text
+}
+
+const documentDidChange = (params: DidChangeTextDocumentParams) => {
+  if (params.contentChanges.length > 0) {
+    DOCUMENTS[params.textDocument.uri] = params.contentChanges[0].text
+  }
+}
+
 // Listen on stdin/stdout
 process.stdin.on("data", (buf) => {
   const message = buf.toString()
   log(message + "\n")
+  log("DOCS: " + JSON.stringify(DOCUMENTS) + "\n")
   const payload = decode(message)
   if (payload !== null) {
     switch (payload.method) {
       case("initialize"):
         respond(payload.id, initializeResult());
+        break;
+      case("document/didOpen"):
+        documentDidOpen(payload.params)
+        break;
+      case("document/didChange"):
+        documentDidChange(payload.params)
         break;
       case("shutdown"):
         respond(payload.id, shutdownResult());
