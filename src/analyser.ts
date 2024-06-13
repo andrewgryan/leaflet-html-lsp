7185c95cb2710987000ae0237d006735844bd1f6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2024-06-13 15:07:08 +0100

    use diagnostic severity

--- src/analyser.ts
+++ src/analyser.ts
@@ -1,5 +1,5 @@
 import { parse } from "node-html-parser"
-import { Diagnostic } from "./diagnostic"
+import { Diagnostic, DiagnosticSeverity } from "./diagnostic"
 
 // Analyse Leaflet-HTML syntax
 export const analyse = (text: string): Diagnostic[] => {
@@ -30,54 +30,21 @@ export const analyse = (text: string): Diagnostic[] => {
   const document = parse(text)
   const els = document.querySelectorAll("l-map")
   els.forEach(el => {
-    console.log(lineNumber)
-    if (!el.hasAttribute("center")) {
-      let i = el.range[0]
-      let j = el.range[0] + el.tagName.length + 1
-      diagnostics.push({
-          range: {
-            start: {line: lineNumber[i], character: characterNumber[i]},
-            end: {line: lineNumber[j], character: characterNumber[j]},
-          },
-          message: "Missing 'center' HTML attribute."
-      })
-    }
-    if (!el.hasAttribute("zoom")) {
-      diagnostics.push({
-          range: {
-            start: {line: lineNumber[el.range[0]], character: characterNumber[el.range[0]]},
-            end: {line: lineNumber[el.range[1] - 1], character: characterNumber[el.range[1] - 1]},
-          },
-          message: "Missing 'zoom' HTML attribute."
-      })
-    }
+    console.log(lineNumber);
+    ["center", "zoom"].forEach(attName => {
+      if (!el.hasAttribute(attName)) {
+        let iStart = el.range[0]
+        let iEnd = el.range[1]
+        diagnostics.push({
+            severity: DiagnosticSeverity.Error,
+            range: {
+              start: {line: lineNumber[iStart], character: characterNumber[iStart]},
+              end: {line: lineNumber[iEnd], character: characterNumber[iEnd]},
+            },
+            message: `Missing '${attName}' HTML attribute.`
+        })
+      }
+    })
   })
-  
-  
-  // // Low-level search
-  // const lines = text.split("\n")
-  // for (let i=0; i<lines.length; i++) {
-  //   const j = lines[i].indexOf("l-map")
-  //   if (j !== -1) {
-  //     if (lines[i].indexOf("center") === -1) {
-  //       diagnostics.push({
-  //           range: {
-  //             start: {line: i, character: j},
-  //             end: {line: i, character: j + 5}
-  //           },
-  //           message: "Missing 'center' HTML attribute."
-  //       })
-  //     }
-  //     if (lines[i].indexOf("zoom") === -1) {
-  //       diagnostics.push({
-  //           range: {
-  //             start: {line: i, character: j},
-  //             end: {line: i, character: j + 5}
-  //           },
-  //           message: "Missing 'zoom' HTML attribute."
-  //       })
-  //     }
-  //   }
-  // }
   return diagnostics
 }
--- src/diagnostic.ts
+++ src/diagnostic.ts
@@ -1,7 +1,17 @@
 export interface Diagnostic {
   range: Range;
+  severity: DiagnosticSeverity;
   message: string;
 }
+
+export namespace DiagnosticSeverity {
+  export const Error: 1 = 1;
+  export const Warning: 2 = 2;
+  export const Information: 3 = 3;
+  export const Hint: 4 = 4;
+}
+
+type DiagnosticSeverity = 1 | 2 | 3 | 4;
 
 interface Range {
   start: Position;
